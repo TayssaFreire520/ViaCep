654daf32bf96b2f86c5b0b26c4025d809a38c256
Tayssa Freire <tayssafreiremesquita@gmail.com>
2026-09-21 10:56:52 -0300

    alteracoes

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,7 +10,6 @@ import { Cep } from './models/cep';
   styleUrl: './app.css',
 })
 export class App {
-
   private cepService = inject(CepService);
 
   cepControl = new FormControl('', [Validators.required, Validators.pattern(/^\d{8}$/)]);
@@ -18,7 +17,6 @@ export class App {
   loading = signal(false);
   erro = signal('');
   buscar() {
-
     if (this.cepControl.invalid) {
       this.erro.set('Digite um CEP válido com 8 números.');
       return;
@@ -28,19 +26,27 @@ export class App {
     this.endereco.set(null);
 
     const cep = this.cepControl.value!;
+
     this.cepService.buscarCep(cep).subscribe({
+      // next -> a resposta da API foi recebida com sucesso
       next: (resposta) => {
-        this.loading.set(false);
+        this.loading.set(false); //parar de exibir a mensagem de aguardando resposta do servidor
+
+        //verifica se a API retornou um erro de usuario que digitou um cep inexistente
         if (resposta.erro) {
           this.erro.set('CEP não encontrado.');
           return;
         }
-        
+
+        //exibe o endereço completo retornado para a API (CEP existente)
         this.endereco.set(resposta);
       },
+
+      //error -> ocorreu algum erro na requisição
       error: () => {
-        this.loading.set(false);
-        this.erro.set('Erro ao consultar API.');
+        this.loading.set(false); //parar de exibir a mensagem de aguardando resposta do servidor
+
+        this.erro.set('Erro ao consultar API.'); //exibe uma mensagem de erro para o usuario
       },
     });
   }
--- src/app/services/cep.ts
+++ src/app/services/cep.ts
@@ -2,14 +2,14 @@ import { inject } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Cep } from '../models/cep';
+import { retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class CepService {
   private http = inject(HttpClient);
   buscarCep(cep: string) {
-    return this.http.get<Cep>(`https://viacep.com.br/ws/${cep}/json/`);
+    return this.http.get<Cep>(`https://viacep.com.br/ws/${cep}/json/`).pipe(retry(2));
   }
 }
